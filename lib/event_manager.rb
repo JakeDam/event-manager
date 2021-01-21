9c354f54e97a5c89a4ec625a675d864dbf58387b
Jake Dam <robert.dam2@gmail.com>
2021-01-20 21:02:39 -0500

    finished phone number assignment

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,21 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def clean_phone_number(number)
+  number = number.to_s.tr('^0-9', '') 
+  if number.length == 10
+    number
+  elsif number.length == 11 && number[0] == "1"
+    number[1..10]
+  else  
+    'Invalid Phone Number'
+  end
 end
 
 def legislators_by_zipcode(zip)
@@ -34,16 +46,25 @@ end
 
 puts 'EventManager initialized.'
 
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
-
+  number = clean_phone_number(row[:homephone])
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id,form_letter)
-  end
+
+  puts "#{name} #{number} #{zipcode}"
 end
